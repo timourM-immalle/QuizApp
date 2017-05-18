81cf134f43d952d7bb972560b610ad5d09a01296
timour.meeusen <timour.meeusen@B209-11>
2017-05-18 13:50:49 +0200

    hhu

---
+++ QuizApp/Meerkeuzevraag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    class Meerkeuzevraag : Vraag
+    {
+        private Random rnd = new Random();
+
+        public int WillekeurigeVraag //Welk type?
+        {
+            get; set;
+        }
+    }
+}
